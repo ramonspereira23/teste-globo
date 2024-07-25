1279180e9bc909ff0e5cfdcb7471e36b79a50955
ramonspereira23 <ramonspereira23@gmail.com>
2024-07-24 22:03:14 -0300

    Update code and Dockerfile

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,10 +21,12 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.modelmapper:modelmapper:3.1.1")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+	implementation("io.micrometer:micrometer-registry-prometheus:1.13.2")
 
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.postgresql:postgresql")
